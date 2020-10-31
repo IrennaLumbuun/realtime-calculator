ab30d6baf55bb428eefaa458af24f9e79c167d5b
irenna lumbuun <irenna.lumbuun@vw.com>
2020-10-30 21:38:06 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import InputBar from './Components/InputBar/InputBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator app</h1>
+      <InputBar/>
+      <div>Placeholder for log viewer</div>
     </div>
   );
 }
